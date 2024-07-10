9c962f7a8360318b3599d13b6175f41f9a0d7c4c
Paul Sparks <psparks1225@gmail.com>
2024-07-10 20:00:58 +0000

    choose a back-end web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("not yet implemented");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(echo))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
