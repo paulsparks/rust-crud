5919fa06f7803e000097d10ae70cac0789390eb3
Paul Sparks <psparks1225@gmail.com>
2024-07-10 13:18:30 +0000

    remove unneccessary code

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,7 @@ use crate::schema::posts;
 use diesel::prelude::*;
 
 #[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::posts)]
+#[diesel(table_name = posts)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Post {
     pub id: i32,
